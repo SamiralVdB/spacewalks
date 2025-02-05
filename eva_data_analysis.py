d2557c638d80be8a4ec67bbd58252364375c6cab
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-02-05 14:35:07 +0100

    updated requirements file

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,62 +1,1 @@
-import json
-import csv
-import datetime as dt
-import matplotlib.pyplot as plt
-
-
-# https://data.nasa.gov/resource/eva.json (with modifications)
-input_file = open('./eva-data.json', 'r')
-output_file = open('./eva-data.csv', 'w')
-graph_file = './cumulative_eva_graph.png'
-
-column_headers = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
-
-eva_data=[]
-
-for i in range(374):
-    line=input_file.readline()
-    print(line)
-    eva_data.append(json.loads(line[1:-1]))
-#data.pop(0)
-## Comment out this bit if you don't want the spreadsheet
-
-w=csv.writer(output_file)
-
-
-time = []
-date =[]
-
-j=0
-for i in eva_data:
-    print(eva_data[j])
-    # and this bit
-    w.writerow(eva_data[j].values())
-    if 'duration' in eva_data[j].keys():
-        tt=eva_data[j]['duration']
-        if tt == '':
-            pass
-        else:
-            t=dt.datetime.strptime(tt,'%H:%M')
-            ttt = dt.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second).total_seconds()/(60*60)
-            print(t,ttt)
-            time.append(ttt)
-            if 'date' in eva_data[j].keys():
-                date.append(dt.datetime.strptime(eva_data[j]['date'][0:10], '%Y-%m-%d'))
-                #date.append(data[j]['date'][0:10])
-
-            else:
-                time.pop(0)
-    j+=1
-
-t=[0]
-for i in time:
-    t.append(t[-1]+i)
-
-date,time = zip(*sorted(zip(date, time)))
-
-plt.plot(date,t[1:], 'ko-')
-plt.xlabel('Year')
-plt.ylabel('Total time spent in space to date (hours)')
-plt.tight_layout()
-plt.savefig(graph_file)
-plt.show()
+import matplotlib.pyplot as pltimport pandas as pd# Data source: https://data.nasa.gov/resource/eva.json (with modifications)input_file = open('./eva-data.json', 'r')output_file = open('./eva-data.csv', 'w')graph_file = './cumulative_eva_graph.png'eva_df = pd.read_json(input_file, convert_dates=['date'])eva_df['eva'] = eva_df['eva'].astype(float)eva_df.dropna(axis=0, inplace=True)eva_df.sort_values('date', inplace=True)eva_df.to_csv(output_file, index=False)eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')plt.xlabel('Year')plt.ylabel('Total time spent in space to date (hours)')plt.tight_layout()plt.savefig(graph_file)plt.show()
